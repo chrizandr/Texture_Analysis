913aa71dfebc28d30f13f7fbedb6e936271b60f5
chrizandr <chris.g14@iiits.in>
2016-12-26 00:29:41 +0530

    working

--- source/segmentation/connected.py
+++ source/segmentation/connected.py
@@ -1,5 +1,5 @@
 import cv2
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import pdb
 import numpy as np
 import os
@@ -16,11 +16,12 @@ def find_position(flag , shape):
 def refine(components):
     useful = list()
     for component in components:
+        comp = cv2.threshold(component , 0 , 1 , cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
         struct = np.ones((3,3),dtype=np.uint8)
-	img = cv2.dilate(1-component,struct,iterations=1)
+        img = cv2.dilate(1-comp,struct,iterations=1)
     	img = cv2.erode(img,struct,iterations=1)
-    	high = (img.shape[0]*img.shape[1])*0.85
-    	low = (img.shape[0]*img.shape[1])*0.15
+    	high = (img.shape[0]*img.shape[1])*0.75
+    	low = (img.shape[0]*img.shape[1])*0.25
     	isum = (1-img).sum()
     	if isum > low and isum < high:
         	useful.append(component)
@@ -39,25 +40,35 @@ def get_connected_components(img):
         min_hor = sub_region[1].min()
         max_ver = sub_region[0].max()
         min_ver = sub_region[0].min()
-        components.append(img[min_ver : max_ver, min_hor :max_hor])
+        region = img[min_ver : max_ver, min_hor :max_hor]
+        if max_hor - min_hor > 3 and max_ver - min_ver > 3:
+            components.append(region)
     return components
 
 def get_text_blocks(components):
     blocks = list()
-    flag = np.zeros((300,300))
-    block = np.ones((300,300))
+    # flag = np.zeros((300,300))
+    block = np.ones((300,300)) * 255
+    x = 0
+    y = 0
     print("Getting blocks")
     for component in components:
-        position = find_position(flag, component.shape)
+        # position = find_position(flag, component.shape)
         if component.shape[0] > 300 or component.shape[1] > 300:
+            print "block left"
             continue
-        if position == -1:
-            blocks.append(block)
-            block = np.ones((300,300))
-            flag = np.zeros((300,300))
-            position = (0,0)
-        block[position[0]:position[0]+component.shape[0] , position[1]:position[1]+component.shape[1]] = component
-        flag[position[0]:position[0]+component.shape[0] , position[1]:position[1]+component.shape[1]] = 1
+        if x + component.shape[1] < 300 and y + component.shape[0] < 300:
+            block[y:y+component.shape[0]]
+
+
+        #
+        # if position == -1:
+        #     blocks.append(block)
+        #     block = np.ones((300,300)) * 255
+        #     flag = np.zeros((300,300))
+        #     position = (0,0)
+        # block[position[0]:position[0]+component.shape[0] , position[1]:position[1]+component.shape[1]] = component
+        # flag[position[0]:position[0]+component.shape[0] , position[1]:position[1]+component.shape[1]] = 1
     return blocks
 
 def get_ids():
@@ -87,11 +98,7 @@ def extract(name):
             writers.write(name[0:-4]+str(count)+','+ label + '\n')
             count += 1
     return None
-# img = cv2.imread("test2.png", 0)
-# img = cv2.threshold(img , 0 , 1 , cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-# components = get_connected_components(img)
-# blocks = get_text_blocks(components)
-# pdb.set_trace()
+
 
 # Optional main function
 data_path = "/home/chris/honours/hand_img/"              # Path of the original dataset
@@ -102,9 +109,12 @@ folderlist = os.listdir(data_path)
 folderlist.sort()
 # Open the output file in write mode
 print("Starting........")
-img = cv2.imread("result.png",0)
+img = cv2.imread("test.png",0)
 components = get_connected_components(img)
 components = refine(components)
+block = get_text_blocks(components)
+pdb.set_trace()
+
 for component in components:
     plt.imshow(component,'gray')
     plt.show()
