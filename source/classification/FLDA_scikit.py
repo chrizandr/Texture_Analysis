9538f2efcb2295b1ebc3c284df2cc9ea9436619d
chrizandr <chris.g14@iiits.in>
2016-12-27 18:58:53 +0530

    working

--- source/classification/FLDA_scikit.py
+++ source/classification/FLDA_scikit.py
@@ -1,9 +1,21 @@
 import pdb
 from sklearn.lda import LDA
 
-filenames = ["Edge_all","Edge_8","Edge_12","Edge_16","Edge_dp_16",]
-for filename in filenames:
-    path = "/home/chris/honours/Texture_Analysis/data_block_csv/Edge/"
+def get_ids(id_file):
+    f = open(id_file,"r")
+    dictionary = dict()
+    for line in f:
+        line = line.strip()
+        line = line.split(",")
+        dictionary[line[0]] = int(line[1])
+    return dictionary
+
+names1 = ["GSCM/GSCM_1","GSCM/GSCM_2","GSCM/GSCM_3","GSCM/GSCM_4","GSCM/GSCM_5","GSCM/GSCM_all"]
+names2 = ["Gabor/Gabor_all", "Gabor/Gabor_4", "Gabor/Gabor_8", "Gabor/Gabor_16", "Gabor/Gabor_32", ]
+names3 = ["Edge/Edge_all","Edge/Edge_8","Edge/Edge_12","Edge/Edge_16","Edge/Edge_dp_16",]
+
+for filename in names1+names2+names3:
+    path = "/home/chris/honours/Texture_Analysis/data_block_csv/"
 
     f = open(path+filename+".csv",'r')
     train_data = list()
@@ -11,12 +23,14 @@ for filename in filenames:
     for line in f:
         l = line.strip()
         l = l.split(',')
-        l = list(map(float , l))
-        train_data.append(l[0:-1])
-        train_class.append(int(l[-1]))
+        ly = list(map(float , l[0:-1]))
+        train_data.append(ly)
+        train_class.append(l[-1])
     f.close()
+    ids = get_ids("/home/chris/honours/text_blocks/writerids.csv")
+    tr_class = [ids[x] for x in train_class]
     clf = LDA()
-    trans = clf.fit_transform(train_data,train_class)
+    trans = clf.fit_transform(train_data,tr_class)
 
     f = open(path+filename+"_LDA.csv",'w')
 
--- source/classification/SVM_voting.py
+++ source/classification/SVM_voting.py
@@ -1,12 +1,29 @@
 import numpy as np
 from sklearn.svm import SVC
 import pdb
+from random import shuffle
+from collections import Counter
+# -----------------------------------------------------
 
+# Getting a list of all the valid writers. Only those in the list will be used.
 f = open("/home/chris/honours/reduced.txt","r")
 writers = list()
 for line in f:
     writers.append(int(line))
 
+# -----------------------------------------------------
+
+def match(tag , names):
+    for i in range(0,3):
+        try:
+            names[tag[0:-i]]
+            return tag[0:-i]
+        except KeyError:
+            continue
+    return -1
+# -----------------------------------------------------
+
+# Getting the class ids of the writers based on the given csv filename. Generates a dictionary
 def get_ids(id_file):
     f = open(id_file,"r")
     dictionary = dict()
@@ -16,6 +33,9 @@ def get_ids(id_file):
         dictionary[line[0]] = line[1]
     return dictionary
 
+# -----------------------------------------------------
+
+# Loads the data stored in a csv file and divides it into the data and the class label
 def load_data(filename):
     global writers
     f = open(filename , 'r')
@@ -30,16 +50,72 @@ def load_data(filename):
             filename.append(l[-1])
     return np.array(data) , filename
 
-def divide_data(data , tags):
-    
+# -----------------------------------------------------
 
+# Divides the data into training and testing. Divides the pages. Later used to divide the blocks
+def divide_data(data , tags):
+    files = get_ids("/home/chris/honours/Texture_Analysis/writerids.csv")
+    test_tags = list()
+    done = list()
+    fil = [x for x in files.iterkeys()]
+    shuffle(fil)
+    for f in fil:
+        if (int(files[f]) not in done) and (int(files[f]) in writers):
+            done.append(int(files[f]))
+            test_tags.append(f) # Test pages
+    return get_blocks(data , tags , test_tags)
+
+# -----------------------------------------------------
+
+# Separates the blocks into training and testing data
+def get_blocks(data , tags , test_tags):
+    test_data = dict()
+    names = dict()
+    train_data = list()
+    train_class = list()
+    ids = get_ids("/home/chris/honours/text_blocks/writerids.csv")
+    for tag in test_tags:
+        test_data[tag] = list()
+        names[tag] = None
+    count = 0
+    for i in range(len(tags)):
+        tag = match(tags[i] , names)
+        if tag != -1:
+            test_data[tag].append(data[i])
+            count +=1
+        else:
+            train_data.append(  data[i])
+            train_class.append(int(ids[tags[i]]))
+    return train_data, train_class , test_data
+
+# -----------------------------------------------------
+
+# Clasiification done using a voting based SVM method
 def classify(filename , writers):
     data , tags = load_data(filename)
-    test , train = divide_data(data , tags)
-    files = np.loadtxt(open("/home/chris/honours/Texture_Analysis/writerids.csv" , "r"), delimiter = ',')
-    pdb.set_trace()
+    ids = get_ids("/home/chris/honours/Texture_Analysis/writerids.csv")
+    final = list()
+    for i in range(2):
+        train_data , train_class , test_data = divide_data(data , tags)
+        train_data = np.array(train_data)
+        train_class = np.array(train_class)
+        svm = SVC()
+        svm.fit(train_data , train_class)
+        correct = 0
+        for page in test_data.iterkeys():
+            if len(test_data[page]) == 0:
+                pdb.set_trace()
+            classid = int(ids[page])
+            result = svm.predict(np.array(test_data[page]))
+            count = Counter(result)
+            label = count.most_common()[0][0]
+            if label == classid:
+                correct += 1
+        final.append((100 * float(correct)) / len(test_data))
+    return sum(final)/len(final)
 
 
+# -------------------------------------------------------------------------------__MAIN__---------------------------------------------
 # data_dir1 = "/home/chris/honours/Texture_Analysis/data_fullimg_csv/"
 data_dir2 = "/home/chris/honours/Texture_Analysis/data_block_csv/"
 names1 = ["GSCM/GSCM_1","GSCM/GSCM_2","GSCM/GSCM_3","GSCM/GSCM_4","GSCM/GSCM_5","GSCM/GSCM_all","GSCM/GSCM_1_LDA","GSCM/GSCM_2_LDA","GSCM/GSCM_3_LDA","GSCM/GSCM_4_LDA","GSCM/GSCM_5_LDA","GSCM/GSCM_all_LDA"]
@@ -51,7 +127,7 @@ for data_dir in [data_dir2]:
     for filename in names2:
         print("Classifying : " + filename)
         # clsf.options = ['-K', '1', '-W', '0' , '-A' ,'weka.core.neighboursearch.KDTree -A "weka.core.EuclideanDistance -R first-last" -S weka.core.neighboursearch.kdtrees.SlidingMidPointOfWidestSide -W 0.01 -L 40 -N']
-        evl = classify(data_dir+"Gabor/Gabor_4"+".csv" , writers)
+        evl = classify(data_dir+"Edge/Edge_all"+".csv" , writers)
         print("Evaluating")
         # ------------------------------------
         results.append((filename.split('/')[1], evl))
