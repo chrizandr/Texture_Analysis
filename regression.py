c220058ee0c7b49e4a01437f936341f253000bb7
chrizandr <chris.g14@iiits.in>
2017-05-30 18:35:54 +0530

    Stroke extraction

--- regression.py
+++ regression.py
@@ -1,45 +1,49 @@
+"""Regressor for estimating stroke curves."""
+
 from sklearn import svm
 import cv2
 import numpy as np
-import pdb
-from scipy import ndimage
 import matplotlib.pyplot as plt
 import os
+import pdb
+
 
 def get_line(X, Y):
+    """Generate equation for a line given a set of points."""
     A = np.vstack([X, np.ones(len(X))]).T
     return np.linalg.lstsq(A, Y)[0]
 
-def feature(img):
-    x,y = (img==0).nonzero()
-    X = [[each] for each in x]
-    clf = svm.SVR(kernel='poly')
-    clf.fit(X, y)
 
-    #A = [0, img.shape[0]]
-    #B1 = [0, img.shape[1]]
-    #B2 = [img.shape[1], 0]
-    #print img.shape
-    #m1, c1 = get_line(A, B1)
-    #m2, c2 = get_line(A, B2)
-    #print m1,c1,m2,c2
+def feature(img):
+    """Genereate curve features."""
+    print("Getting points...")
+    y, x = (img == 0).nonzero()
+    print("Training...")
+    X = x.reshape(-1, 1)
+    clf = svm.SVR(kernel='rbf', C=1e3, gamma=0.1)
+    clf.fit(X, y)
+    print("Predicting...")
     point_y = []
     point_x = []
-    for i in range(0,img.shape[0],img.shape[0]/10):
+    for i in np.linspace(0, img.shape[1], 100):
         point_x.append(i)
         point_y.append(clf.predict(i))
-
-    plt.scatter(x,y)
-    plt.scatter(point_x, point_y, color = 'red')
+    plt.scatter(x, y)
+    plt.scatter(point_x, point_y, color='red')
     plt.show()
+    return point_y
 
 
-data_path = "/home/sanny/Documents/clustering_model/data/"
-folderlist = os.listdir(data_path)
-folderlist.sort()
+img = cv2.imread("t1.tiff", 0)
+f = feature(img)
+img = cv2.imread("t2.tiff", 0)
+f = feature(img)
+# data_path = "/home/sanny/Documents/clustering_model/data/"
+# folderlist = os.listdir(data_path)
+# folderlist.sort()
 
-for name in folderlist:
-    if name[-4:]=='.png':
-        print("Processing "+ name)
-        img = cv2.imread(data_path+name, 0)
-        feature(img)
+# for name in folderlist:
+#     if name[-4:]=='.png':
+#         print("Processing "+ name)
+#         img = cv2.imread(data_path+name, 0)
+#         feature(img)
