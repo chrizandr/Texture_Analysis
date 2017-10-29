635efabcba70f149812ca95aeb15abdd1d6d08d4
Chris Andrew <chris.g14@iiits.in>
2017-10-29 15:51:28 +0530

    Compiling model and running checkpoints.

--- source/deep_color/vgg_basic.py
+++ source/deep_color/vgg_basic.py
@@ -15,6 +15,7 @@ from keras.layers import GlobalMaxPooling2D
 from keras.layers import GlobalAveragePooling2D
 from keras.preprocessing import image
 from keras import backend as K
+from keras.callbacks import ModelCheckpoint
 from keras.applications.imagenet_utils import decode_predictions
 from keras.applications.imagenet_utils import preprocess_input
 from keras.applications.imagenet_utils import _obtain_input_shape
@@ -23,8 +24,61 @@ from keras.layers import Flatten
 from keras.layers import Dense
 
 
-WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
-WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+class DataGenerator(object):
+    """Generates data for Keras."""
+
+    def __init__(self, dim_x=224, dim_y=224, dim_z=3, batch_size=32, shuffle=True, n_classes=150, list_IDs=[], labels=[], filepath=""):
+        """Initialization."""
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.dim_z = dim_z
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.n_classes = n_classes
+        self.list_IDs = list_IDs
+        self.labels = labels
+
+    def generate(self, labels):
+        """Generate batches of samples."""
+        while 1:
+            indexes = self.get_exploration_order(self.list_IDs)
+
+            imax = int(len(indexes)/self.batch_size)
+            for i in range(imax):
+                list_IDs_temp = [self.list_IDs[k] for k in indexes[i*self.batch_size:(i+1)*self.batch_size]]
+                X, y = self.data_generation(labels, list_IDs_temp)
+
+                yield X, y
+
+    def get_exploration_order(self):
+        """Generate order of exploration."""
+        indexes = np.arange(len(self.list_IDs))
+        if self.shuffle:
+            np.random.shuffle(indexes)
+
+        return indexes
+
+    def data_generation(self, labels, list_IDs_temp):
+        """Generate data of batch_size samples."""
+        X = np.empty((self.batch_size, 1, self.dim_x, self.dim_y, self.dim_z))
+        y = np.empty((self.batch_size), dtype=int)
+
+        for i, ID in enumerate(list_IDs_temp):
+            img = image.load_img(filepath + ID, target_size=(224, 224))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+            X[i, 0, :, :, :] = x
+            name = ID.split('-')[0] + '-' + ID.split('-')[1]
+            y[i] = labels[name]
+
+        return X, self.sparsify(y)
+
+    def sparsify(self, y):
+        """Return labels in binary NumPy array."""
+        # y[i] == j+1 if labels start from 0, else y[i] == j
+        return np.array([[1 if y[i] == j+1 else 0 for j in range(self.n_classes)]
+                         for i in range(y.shape[0])])
 
 
 def getIds(filename):
@@ -37,7 +91,7 @@ def getIds(filename):
     return ids
 
 
-def VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000):
+def VGG_Writer(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000):
     """VVG16 without final dense layers."""
     # Determine proper input shape
     input_shape = _obtain_input_shape(input_shape,
@@ -94,36 +148,11 @@ def VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=N
     else:
         inputs = img_input
     # Create model.
-    model = Model(inputs, x, name='vgg16')
+    feature_part = Model(inputs, x, name='vgg16')
 
     weights_path = 'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
-    model.load_weights(weights_path)
-
-    return model, input_shape
-
-
-def read_data(datapath):
-    """Read the data."""
-    writer_ids = getIds("/home/chrizandr/data/Telugu/writerids.csv")
-    X = list()
-    y = list()
-    folderlist = os.listdir(datapath)
-    for f in folderlist:
-        img = image.load_img(datapath + f, target_size=(224, 224))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-        X.append(x)
-        name = f.split('-')[0] + '-' + f.split('-')[1]
-        y.append(writer_ids[name])
-
-    X = np.array(X)
-    return X, y
-
-
-if __name__ == '__main__':
-    feature_part, input_shape = VGG16(include_top=False, weights='imagenet')
+    feature_part.load_weights(weights_path)
 
     img_input = Input(shape=(224, 224, 3))
     features = feature_part(img_input)
@@ -134,9 +163,49 @@ if __name__ == '__main__':
 
     model = Model(img_input, predictions, name="classifier")
 
-    X, y = read_data("/home/chrizandr/data/Telugu/test/")
-    pdb.set_trace()
-    print('Input image shape:', x.shape)
+    return model
 
-    preds = model.predict(x)
-    print('Predicted:', decode_predictions(preds))
+
+def create_Partition(filepath, labels):
+    """Create training and validation sets from files."""
+    data = dict()
+    x = os.listdir(filepath)
+    data["training"] = x
+
+    validation = list()
+    for l in labels:
+        for f in x:
+            name = f.split('-')[0] + '-' + f.split('-')[1]
+            if name == l:
+                validation.append(f)
+                break
+
+    data["validation"] = validation
+
+    return data
+
+
+if __name__ == '__main__':
+    filepath = "/home/chrizandr/data/Telugu/test/"
+    class_path = "/home/chrizandr/data/writerids.csv"
+    output_file = "/home/chrizandr/data/VGG_Writer.hd5"
+    batch_size = 32
+
+    labels = getIds(class_path)
+    data = create_Partition(filepath, labels)
+
+    checkpoint = ModelCheckpoint(filepath=output_file, monitor='val_loss')
+
+    model = VGG_Writer(include_top=False, weights='imagenet')
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    training_generator = DataGenerator(dim_x=224, dim_y=224, dim_z=3, batch_size=batch_size, shuffle=True,
+                                       n_classes=150, list_IDs=data["training"], labels=labels, filepath=filepath)
+
+    validation_generator = DataGenerator(dim_x=224, dim_y=224, dim_z=3, batch_size=batch_size, shuffle=True,
+                                         n_classes=150, list_IDs=data["validation"], labels=labels, filepath=filepath)
+
+    model.fit_generator(generator=training_generator,
+                        steps_per_epoch=len(data['train'])//batch_size,
+                        validation_data=validation_generator,
+                        validation_steps=len(data['validation'])//batch_size)
